9ec17786f98f4761589c4a0ced2eafb56d3c6ebf
colewalker <cmw9nb@mst.edu>
2023-03-13 11:50:08 -0500

    Started implementing iterativeMethod, named functions for CarrySelect adn FourBitFullAdder which need to be implemented

--- simulator-main.py
+++ simulator-main.py
@@ -1,8 +1,116 @@
 # Add and Shift helper function
+import math
+
+
+# Need to implement
+def FourBitFullAdder(Abits, Bbits, carryIn):
+    return
+
+
+# Need to implement
+def CarrySelect(binaryNumOne, binaryNumTwo):
+    return
+
+
+# Initialize the starting registers to be 2^n
+def iterativeMethodPrep(A, B):
+    # Set n (should be of 2^x)
+    n = math.ceil(math.log(max(len(A), len(B)), 2))
+    lenIter = int(math.pow(2, n))
+
+    # Convert A and B into n bit list
+    A_Init = [0] * (lenIter - len(A)) + A[:]
+    B_Init = [0] * (lenIter - len(B)) + B[:]
+
+    return A_Init, B_Init, n
+
+
+# Base case for iterative method (4-bits)
+def iterativeBase(A, B, n):
+    # 2^n (ac) operation
+    ac_0 = AND(A[2], B[2])
+    ac_1, carryAC1 = XOR(AND(A[3], B[2]), AND(A[2], B[3]))
+    ac_2, carryAC2 = XOR(AND(A[3], B[3]), carryAC1)
+    ac_3 = carryAC2
+
+    AC = [ac_3, ac_2, ac_1, ac_0]
+
+    # 2 ^n/2 (ad + bc)
+    ad_0 = AND(A[2], B[0])
+    ad_1, carryAD1 = XOR(AND(A[3], B[0]), AND(A[2], B[1]))
+    ad_2, carryAD2 = XOR(AND(A[3], B[1]), carryAD1)
+    ad_3 = carryAD2
+
+    bc_0 = AND(A[0], B[2])
+    bc_1, carryBC1 = XOR(AND(A[1], B[2]), AND(A[0], B[3]))
+    bc_2, carryBC2 = XOR(AND(A[1], B[3]), carryBC1)
+    bc_3 = carryBC2
+
+    # + bd
+    bd_0 = AND(A[0], B[0])
+    bd_1, carryBD1 = XOR(AND(A[1], B[0]), AND(A[0], B[1]))
+    bd_2, carryBD2 = XOR(AND(A[1], B[1]), carryBD1)
+    bd_3 = carryBD2
+
+    BD = [bd_3, bd_2, bd_1, bd_0] + [0] * 4
+
+    # ADD BC + AD
+    # Fast addition Carry Select Adder
+
+    # Put AC, BD in same register
+    AC_BD = AC + BD
+
+    return
+
+def iterativeMethod(A, B, n):
+    # Base case
+    if n == 4:
+        return iterativeBase(A, B, n)
+    else:
+        # Recursive call
+        # 2^n iter(ac, n/2)
+
+        # 2^n/2 (iter(ad, n/2) + iter(bc, n/2)
+
+        # + iter(bd, n/2)
+        return
+
+
+# AND Operation
+# Input: Two Bits
+# Output: 1 or 0
+def AND(bitOne, bitTwo):
+    if bitOne == 1 and bitTwo == 1:
+        return 1
+    else:
+        return 0
+
+
+# NAND Operation
+# Input: Two Bits
+# Output: 1 or 0
+def NAND(bitOne, bitTwo):
+    if bitOne == 1 and bitTwo == 1:
+        return 0
+    else:
+        return 1
+
+
+# OR Operation
+# Input: Two Bits
+# Output: 1 or 0
+def OR(bitOne, bitTwo):
+    if bitOne or bitTwo:
+        return 1
+    else:
+        return 0
+
+
 # XOR Operation
 # Input: Two Bits
 # Output: Result bit and carry
 def XOR(bitOne, bitTwo):
+    # return AND(OR(bitOne, bitTwo), NAND(bitOne, bitTwo))
     # 1 1 or 0 0
     if bitOne == bitTwo:
         # If 1 1 else 0 0
@@ -28,6 +136,7 @@ def shiftAQ(AQ):
     return AQ
 
 
+# Add and Shift helper function
 # Add B to AQ Function
 def add_B_to_AQ(AQ, B):
     sizeAQ = len(AQ)-1
@@ -94,8 +203,8 @@ def add_and_shift(multiplier, multiplicand):
     return AQ, num_XOR, num_Shift
 
 
-test_multiplier = [0, 0, 1, 0]
-test_multiplicand = [1, 1, 0, 0]
+test_multiplier = [0, 1, 0, 1]
+test_multiplicand = [1, 1, 0, 1]
 
 print(test_multiplier, "*", test_multiplicand, '=')
 
@@ -106,3 +215,9 @@ mult_result, numXOR, numShift = add_and_shift(test_multiplier, test_multiplicand
 
 print("Result: ", mult_result)
 print("Timing: ", 2 * numXOR + 3 * numShift, "DT")
+
+
+print("Iterative Method Testing")
+A_Test = [0, 0, 1, 1, 0, 1]
+B_Test = [0, 0, 1, 1, 1, 1]
+print(iterativeMethod(iterativeMethodPrep(A_Test, B_Test)))
