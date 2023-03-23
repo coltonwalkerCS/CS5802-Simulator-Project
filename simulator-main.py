5c7cd8b40c15f947aa599963b95109731c9e5809
ormd3n <ormd3n@umsystem.edu>
2023-03-23 15:24:01 -0500

    Tidied up comments

--- simulator-main.py
+++ simulator-main.py
@@ -7,8 +7,6 @@ time = 0
 ------------ NOTES ------------
 '''
 
-# TODO: Fix IterativeMethod() - Not producing right product
-# TODO: Fix FastAdderCarrySelect() - ???
 # TODO: Create a way to keep track of how much time the methods take
 
 """
@@ -70,17 +68,6 @@ def XOR(bitOne: int, bitTwo: int) -> Literal[0, 1]:
     global time
     time += 2
     return AND(OR(bitOne, bitTwo), NAND(bitOne, bitTwo))
-
-
-# Given two binary numbers with carry 1 and 0
-# choose the binary option which corresponds
-# to the carry
-# def MUX(binOptionCarryOne, binOptionCarryZero, carryOut):
-#     if carryOut:
-#         return binOptionCarryOne
-#     else:
-#         return binOptionCarryZero
-
 
 # Four bit full adder implementation
 # Using logic gates to generate results
@@ -345,108 +332,10 @@ def add_and_shift(multiplier, multiplicand):
 -------- ITERATIVE METHOD ALGORITHM --------
 """
 
-
-# Initialize the starting registers to be 2^n
-def iterativeMethodPrep(A, B):
-    # Set n (should be of 2^x)
-    n = math.ceil(math.log(max(len(A), len(B)), 2))
-    lenIter = int(math.pow(2, n))
-
-    # Convert A and B into n bit list
-    A_Init = [0] * (lenIter - len(A)) + A[:]
-    B_Init = [0] * (lenIter - len(B)) + B[:]
-
-    return A_Init, B_Init, n
-
-
-# Base case for iterative method (4-bits)
-def iterativeBase(A, B):
-    # 2^n (ac) operation
-    ac_0 = AND(A[2], B[2])
-    ac_1, carryAC1 = XOR(AND(A[3], B[2]), AND(A[2], B[3])), AND(AND(A[3], B[2]), AND(A[2], B[3]))
-    ac_2, carryAC2 = XOR(AND(A[3], B[3]), carryAC1), AND(AND(A[3], B[3]), carryAC1)
-    ac_3 = carryAC2
-
-    AC = [ac_3, ac_2, ac_1, ac_0]
-
-    # 2 ^n/2 (ad + bc)
-    ad_0 = AND(A[2], B[0])
-    ad_1, carryAD1 = XOR(AND(A[3], B[0]), AND(A[2], B[1])), AND(AND(A[3], B[0]), AND(A[2], B[1]))
-    ad_2, carryAD2 = XOR(AND(A[3], B[1]), carryAD1), AND(AND(A[3], B[1]), carryAD1)
-    ad_3 = carryAD2
-
-    AD = [ad_3, ad_2, ad_1, ad_0]
-
-    bc_0 = AND(A[0], B[2])
-    bc_1, carryBC1 = XOR(AND(A[1], B[2]), AND(A[0], B[3])), AND(AND(A[1], B[2]), AND(A[0], B[3]))
-    bc_2, carryBC2 = XOR(AND(A[1], B[3]), carryBC1), AND(AND(A[1], B[3]), carryBC1)
-    bc_3 = carryBC2
-
-    BC = [bc_3, bc_2, bc_1, bc_0]
-
-    # + bd
-    bd_0 = AND(A[0], B[0])
-    bd_1, carryBD1 = XOR(AND(A[1], B[0]), AND(A[0], B[1])), AND(AND(A[1], B[0]), AND(A[0], B[1]))
-    bd_2, carryBD2 = XOR(AND(A[1], B[1]), carryBD1), AND(AND(A[1], B[1]), carryBD1)
-    bd_3 = carryBD2
-
-    BD = [bd_3, bd_2, bd_1, bd_0]
-
-    # ADD BC + AD
-    # with fast addition Carry Select Adder
-    BC_AD, carryBCAD = FastAdderCarrySelect(BC, AD, 0)
-    # 2^n/2 (add two 00)
-    BC_AD = BC_AD + [0, 0]
-    BC_AD = [carryBCAD] + BC_AD
-    # Fast addition Carry Select Adder
-    for _ in range(8-len(BC_AD)):
-        BC_AD.insert(0, 0)
-
-    # Put AC, BD in same register
-    AC_BD = AC + BD
-    # Add them together and return the result
-    return FastAdderCarrySelect(AC_BD, BC_AD, 0)
-
-
-# NEED TO FIX:
-# ONCE FAST ADDER IS FIXED SHOULD BE WORKING WITH
-# NEW FAST ADDER IMPLEMENTATION
-# ITS TOO CONFUSING ATM WITH THE CARRY
-
-# Using X * Y = 2^n(ac) + 2^(n/2)(ad + bc) + bd, we can simplify the algorithm
-# 2^nX is the same as shifting a binary number, X, n spaces to the left
-# Therefore, find AD, BC, BD, and AC and follow the equation 
-def iterativeMethod(A, B, n):
-    # Base case
-    if n % 4 == 0:
-        return iterativeBase(A, B)
-    else:
-        # Recursive call
-        n_div_two = int(n / 2)
-        # 2^n iter(ac, n/2)
-        AC, AC_Carry = iterativeMethod(A[0:n_div_two], B[0:n_div_two], n_div_two)
-
-        # 2^n/2 (iter(ad, n/2) + iter(bc, n/2)
-        AD, AD_Carry = iterativeMethod(A[0:n_div_two], B[n_div_two:n], n_div_two)
-        BC, BC_Carry = iterativeMethod(A[n_div_two:n], B[0:n_div_two], n_div_two)
-
-        # Take care of AD and BC Carry
-        AD = [0, 0, 0] + [AD_Carry] + AD
-        BC = [0, 0, 0] + [BC_Carry] + BC
-
-        AD_BC, ADBC_Carry = FastAdderCarrySelect(AD, BC, 0)
-        AD_BC = AD_BC + [0, 0, 0, 0]
-
-        # + iter(bd, n/2)
-        BD, BD_Carry = iterativeMethod(A[n_div_two:n], B[n_div_two:n], n_div_two)
-
-        # Fast adder
-
-        AC_BD = AC + BD
-        # Make sure to implement w/ carry
-
-        return FastAdderCarrySelect(AC_BD, AD_BC, 0)
-
+# Base case for iterative function
+# Spliting terms until the length is two
+# We then can do trivial multiplication (0 * 1 = 0 or 1 * 1 = 1)
+# Returning 2^n*(ac) + 2^(n/2)(adbc) + bd
 def base(A: List[int], B: List[int], n: int) -> List[int]:
     a: int = A[0]
     b: int = A[1]
@@ -490,38 +379,24 @@ def base(A: List[int], B: List[int], n: int) -> List[int]:
     to_return, carry_one = FastAdderCarrySelect(ac, adbc, 0)
     to_return, carry_two = FastAdderCarrySelect(to_return, bd, carry_one)
 
-    # if carry_one and carry_two:
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 1)
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 0)
-    # elif carry_one or carry_one:
-    #     to_return.insert(0, 1)
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 0)
-    # else:
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 0)
-    #     to_return.insert(0, 0)
-
     return to_return, carry_two
 
 
-
-def it(A: List[int], B: List[int], n: int) -> List[int]:
+# Wrapper function for itMain
+# It makes sure the length of the binary number is a power of two
+# Trims the zeros at beginning of result as well
+def iterative(A: List[int], B: List[int], n: int) -> List[int]:
     while not math.log(n, 2).is_integer():
         A.insert(0, 0)
         B.insert(0, 0)
         n += 1
-    # if len(A) % 4 != 0:
-    #     for _ in range(4 - (len(A) % 4)):
-    #         A.insert(0, 0)
-    #         B.insert(0, 0)
-    #         n += 1
     
-    return itMain(A, B, n)
+    result = itMain(A, B, n)
+
+    while result[0][0] == 0:
+        result[0].pop(0)
+    
+    return result
 
 def itMain(A: List[int], B: List[int], n: int) -> List[int]:
     if n == 2:
@@ -529,6 +404,7 @@ def itMain(A: List[int], B: List[int], n: int) -> List[int]:
     
     # Recursive call
     n_div_two = n // 2
+
     # 2^n iter(ac, n/2)
     AC, AC_Carry = itMain(A[0:n_div_two], B[0:n_div_two], n_div_two)
     AC = SHIFTL(AC, n)
@@ -556,203 +432,6 @@ def itMain(A: List[int], B: List[int], n: int) -> List[int]:
     # Make sure to implement w/ carry
 
     return FastAdderCarrySelect(AC_BD, AD_BC, ACBD_Carry)
-
-def summandMatrix(A: List[int], B: List[int]) -> List[List[int]]:
-    num_length = len(A)
-    columns = (2 * num_length) - 1
-    append = []
-    summand_matrix = []
-
-    for i in range(num_length):
-        if B[num_length - (1 + i)] == 0:
-            append = [0] * columns
-        else:
-            append = [0] * ((num_length - 1) - i)
-            for j in range(num_length):
-                append.append(A[j])
-            
-            for j in range(i):
-                append.append(0)
-        
-        summand_matrix.append(append)
-        append = []
-
-    return summand_matrix
-
-
-def iterative(A: List[int], B: List[int]) -> List[int]:
-    summand_matrix: List[List[int]] = []
-    n: int = len(A)
-    temp_int: int = 0
-
-    # A * B = 2^n(ac) + 2^(n/2)(ad + bc) + bd
-    ad: List[List[int]] = []
-    bd: List[List[int]] = []
-    ac: List[List[int]] = []
-    bc: List[List[int]] = []
-
-    ad_bin: List[int] = []
-    bd_bin: List[int] = []
-    ac_bin: List[int] = []
-    bc_bin: List[int] = []
-    adbc_bin: List[int] = []
-    temp_bin: List[int] = []
-    return_bin: List[int] = []
-
-    # Helps populate summand_matrix
-    append: List[int] = []
-
-    ac_shift: List[int] = []
-    adbc_shift: List[int] = []
-
-    # Length of the numbers needs to be divisible by 2
-    # If a number is not divisible by 2, adding 1 will ALWAYS make it divisible by 2
-    # Adding 0 to beginning of binary number does not change its value
-    if n % 2 != 0:
-        A.insert(0, 0)
-        B.insert(0, 0)
-        n += 1
-    
-    # Create summand matrix from input
-    summand_matrix = summandMatrix(A, B)
-
-    # Filling ad, bd, ac, bc from summand_matrix
-    for i in range((n // 2)):
-        append = [0] * ((n // 2) - i - 1)
-
-        for j in range((n // 2)):
-            append.append(summand_matrix[i][((n - 1) + ((n // 2) + j - i))])
-
-        for j in range(i):
-            append.append(0)
-
-        bd.append(append)
-        append = []
-
-    for i in range((n // 2)):
-        append = [0] * ((n // 2) - i - 1)
-
-        for j in range((n // 2)):
-            append.append(summand_matrix[i][((n - 1) - i + j)])
-
-        for j in range(i):
-            append.append(0)
-
-        ad.append(append)
-        append = []
-
-    for i in range((n // 2)):
-        append = [0] * ((n // 2) - i - 1)
-
-        for j in range((n // 2)):
-            append.append(summand_matrix[(i + (n // 2))][((n - (n // 2) - 1) - i + j)])
-
-        for j in range(i):
-            append.append(0)
-
-        ac.append(append)
-        append = []
-
-    for i in range((n // 2)):
-        append = [0] * ((n // 2) - i - 1)
-
-        for j in range((n // 2)):
-            append.append(summand_matrix[(i + (n // 2))][((n - 1) + (j - i))])
-
-        for j in range(i):
-            append.append(0)
-
-        bc.append(append)
-        append = []
-
-    if len(ad[0]) % 4 != 0:
-        for i in range(len(ad)):
-            for _ in range(4 - (len(ad[i]) % 4)):
-                ad[i].insert(0, 0)
-                ac[i].insert(0, 0)
-                bc[i].insert(0, 0)
-                bd[i].insert(0, 0)
-    
-    temp_bin = ad[0]
-    for i in range(len(ad) - 1):
-        temp_bin, temp_int = FastAdderCarrySelect(temp_bin, ad[i + 1], temp_int)
-    ad_bin = temp_bin
-    
-    if temp_int == 1:
-        ad.insert(0, 1)
-        temp_int = 0
-
-    temp_bin = ac[0]
-    for i in range(len(ad) - 1):
-        temp_bin, temp_int = FastAdderCarrySelect(temp_bin, ac[i + 1], temp_int)
-    ac_bin = temp_bin
-
-    if temp_int == 1:
-        ac.insert(0, 1)
-        temp_int = 0
-
-    temp_bin = bd[0]
-    for i in range(len(ad) - 1):
-        temp_bin, temp_int = FastAdderCarrySelect(temp_bin, bd[i + 1], temp_int)
-    bd_bin = temp_bin
-
-    if temp_int == 1:
-        bd.insert(0, 1)
-        temp_int = 0
-
-    temp_bin = bc[0]
-    for i in range(len(ad) - 1):
-        temp_bin, temp_int = FastAdderCarrySelect(temp_bin, bc[i + 1], temp_int)
-    bc_bin = temp_bin
-
-    if temp_int == 1:
-        bc.insert(0, 1)
-        temp_int = 0
-
-    if len(ad_bin) % 4 != 0:
-        for _ in range(4 - (len(ad_bin) % 4)):
-            ad_bin.insert(0, 0)
-    
-    if len(bc_bin) % 4 != 0:
-        for _ in range(4 - (len(bc_bin) % 4)):
-            bc_bin.insert(0, 0)
-
-    if len(ad_bin) < len(bc_bin):
-        for _ in range(len(bc_bin) - len(ad_bin)):
-            ad_bin.insert(0, 0)
-    
-    if len(bc_bin) < len(ad_bin):
-        for _ in range(len(ad_bin) - len(bc_bin)):
-            bc_bin.insert(0, 0)
-    
-    adbc_bin, temp_int = FastAdderCarrySelect(ad_bin, bc_bin, 0)
-
-    if temp_int == 1:
-        adbc_bin.insert(0, 1)
-        temp_int = 0
-
-    ac_shift = SHIFTL(ac_bin, n)
-    adbc_shift = SHIFTL(adbc_bin, (n // 2))
-
-    if len(ac_shift) % 4 != 0:
-        for _ in range(4 - (len(ac_shift) % 4)):
-            ac_shift.insert(0, 0)
-    
-    while len(adbc_shift) != len(ac_shift):
-        adbc_shift.insert(0, 0)
-
-    while len(bd_bin) != len(ac_shift):
-        bd_bin.insert(0, 0)
-    
-    temp_bin = []
-    temp_bin, temp_int = FastAdderCarrySelect(ac_shift, adbc_shift, 0)
-    return_bin, temp_int = FastAdderCarrySelect(temp_bin, bd_bin, temp_int)
-
-    if temp_int == 1:
-        return_bin.insert(0, 1)
-
-    return return_bin
-
 
 # Shift Left
 # Input: Binary number and number of shifts
@@ -803,5 +482,5 @@ if __name__ == "__main__":
     for multPair in testData:
         time = 0
         # print(multPair[0], '+', multPair[1], '=', iterative(multPair[0], multPair[1]))
-        print(multPair[0], '+', multPair[1], '=', it(multPair[0], multPair[1], len(multPair[0])))
+        print(multPair[0], '+', multPair[1], '=', iterative(multPair[0], multPair[1], len(multPair[0])))
         print('time of operation: ', time, end='\n')
